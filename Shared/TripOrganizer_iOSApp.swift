3d7cb26ed6141916930fbed3ce5f4199af441979
System Administrator <root@Pavels-MacBook-Pro.local>
2021-10-28 23:42:31 +0300

    First commit. Created Universal App project in Xcode.

---
+++ Shared/TripOrganizer_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripOrganizer_iOSApp.swift
+//  Shared
+//
+//  Created by Pavel Palancica on 10/28/21.
+//
+
+import SwiftUI
+
+@main
+struct TripOrganizer_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
